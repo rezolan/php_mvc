5c284d6cb723103db69867320cf3c742bf6d18bd
rezolan <alexei.kravchenko@ecocoach.com>
2018-09-27 20:19:36 +0300

    fix error

--- controllers/NewsController.php
+++ controllers/NewsController.php
@@ -4,13 +4,13 @@ class NewsController {
 		include ROOT . '/views/news/newsView.php';
 	}
 	public function actionList($params) {
-		include ROOT . 'models/News.php';
+		include ROOT . '/models/News.php';
 		if($params[0]) {
 			$data = News::getNewsItemById($params[0]);
-			include ROOT . 'views/news/newsListItem.php';
+			include ROOT . '/views/news/newsListItem.php';
 		} else {
 			$data = News::getNewsList();
-			include ROOT . 'views/news/newsList.php';
+			include ROOT . '/views/news/newsList.php';
 		}
 	}
 	public function actionSingle() {
