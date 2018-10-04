90296dba893e448e83dc9010260e63fb7280d205
rezolan <alexei.kravchenko@ecocoach.com>
2018-10-04 20:46:00 +0300

    finished pagination

--- components/Pagination.php
+++ components/Pagination.php
@@ -10,14 +10,54 @@ class Pagination {
 		$this->paginationPath = $paginationPath;
 	}
 	public function show() {
-		$x = "<div style='display:flex;justify-content:center;'>";
-		for($i = 1; $i <= $this->lihksCount; $i++){
+		if($this->activePage > 3 AND $this->activePage < $this->lihksCount - 2) {
+			$min = $this->activePage - 2;
+			$max = $this->activePage + 2;
+			$leftArrow = "<a href='".$this->paginationPath."1'>
+							<button>First</button>
+						</a>
+						<a href='".$this->paginationPath.($this->activePage-1)."'>
+							<button>Prev</button>
+						</a>";
+			$rightArrow = "<a href='".$this->paginationPath.($this->activePage+1)."'>
+								<button>Next</button>
+							</a>
+							<a href='".$this->paginationPath.$this->lihksCount."'>
+								<button>Last</button>
+							</a>";
+		} else if($this->activePage <= 3) {
+			$min = 1;
+			$max = 5;
+			$leftArrow = "";
+			$rightArrow = "<a href='".$this->paginationPath.($this->activePage+1)."'>
+								<button>Next</button>
+							</a>
+							<a href='".$this->paginationPath.$this->lihksCount."'>
+								<button>Last</button>
+							</a>";
+		} else {
+			$min = $this->lihksCount - 4;
+			$max = $this->lihksCount;	
+			$leftArrow = "<a href='".$this->paginationPath."1'>
+								<button>First</button>
+							</a>
+							<a href='".$this->paginationPath.($this->activePage-1)."'>
+								<button>Prev</button>
+							</a>";
+			$rightArrow = "";
+		}
+	
+		$x = "<div class='pagination'>";
+		$x .= $leftArrow;
+		
+		for($i = $min; $i <= $max; $i++){
 			if($this->activePage==$i) {
-				$x.="<button href='".$this->paginationPath."$i'>$i</button>";
+				$x.="<button class='active'>$i</button>";
 			} else {
-				$x.="<a href='".$this->paginationPath."$i'>$i</a>";
+				$x.="<a href='".$this->paginationPath."$i'><button>$i</button></a>";
 			}
 		}
+		$x .= $rightArrow;
 		$x.="</div>";
 		return $x;
 		// return (
