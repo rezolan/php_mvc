7ea0e9213e4271457c3edf17eea322dca548610a
rezolan <alexei.kravchenko@ecocoach.com>
2018-10-09 20:33:03 +0300

    fixed 404 page

--- config/routes.php
+++ config/routes.php
@@ -1,12 +1,12 @@
 <?php
 return array(
-	'/exit' => 'registration/destroy',
-	'/login' => 'registration/autorisation',
-	'/registration' => 'registration/register',
+	'/exit/' => 'registration/destroy',
+	'/login/' => 'registration/autorisation',
+	'/registration/' => 'registration/register',
 	'/product/([0-9]+)' => 'product/product/$1',
 	'/([0-9]+)/page-([0-9]+)' => 'product/index/$1/$2',
 	'/page-([0-9]+)' => 'product/index//$1',
 	'/([0-9]+)' => 'product/index/$1',
 	'/news' => 'news/list',
-	'/[a-zA-Z/0-9\-]+' => 'error/notfound',
+	'/[a-zA-Z/0-9\-.]+' => 'error/notfound',
 	'/' => 'product/index');
