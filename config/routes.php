1c415fb64627cfbbb8c326ef13eb7c3f85c2c2f4
rezolan <alexei.kravchenko@ecocoach.com>
2018-10-11 14:49:46 +0300

    changed routes

--- config/routes.php
+++ config/routes.php
@@ -1,8 +1,8 @@
 <?php
 return array(
-	'/exit/' => 'registration/destroy',
-	'/login/' => 'registration/autorisation',
-	'/registration/' => 'registration/register',
+	'/exit' => 'registration/destroy',
+	'/login' => 'registration/autorisation',
+	'/registration' => 'registration/register',
 	'/product/([0-9]+)' => 'product/product/$1',
 	'/([0-9]+)/page-([0-9]+)' => 'product/index/$1/$2',
 	'/page-([0-9]+)' => 'product/index//$1',
